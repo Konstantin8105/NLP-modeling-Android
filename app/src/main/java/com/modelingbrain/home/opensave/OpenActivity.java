78a8e893a8b219e8523bb1dd77a23e7c3e7b22fd
konstantin8105 <konstantin8105@gmail.com>
2016-10-02 22:14:21 +0300

    bug fix - add permition for Android 6.0 for open files

--- app/src/main/java/com/modelingbrain/home/opensave/OpenActivity.java
+++ app/src/main/java/com/modelingbrain/home/opensave/OpenActivity.java
@@ -1,8 +1,13 @@
 package com.modelingbrain.home.opensave;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 
@@ -24,6 +29,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OpenActivity extends SaveOpenActivity {
+
+    private static final int REQUEST_READWRITE_STORAGE = 1000;
+
     @SuppressWarnings("unused")
     private final String TAG = this.getClass().toString();
 
@@ -31,6 +39,54 @@ public class OpenActivity extends SaveOpenActivity {
     protected void initializeTask() {
         task = new OpenTask();
         task.execute();
+    }
+
+    //TODO https://developer.android.com/training/permissions/requesting.html
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READWRITE_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+    private void findAllFiles(List<String> fileList, String pattern) {
+        Log.d(TAG, "findAllFiles - start");
+        File sdPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        Log.d(TAG, "sdPath = " + sdPath + " :isExist = " + sdPath.exists() + " : isDirectory = " + sdPath.isDirectory());
+        File[] files = sdPath.listFiles();
+        if (files == null) {
+            Log.d(TAG, "findAllFiles - finish - No files");
+            return;
+        }
+        for (File file : files) {
+            if (!file.isDirectory()) {
+                if (file.getName().endsWith(pattern.toLowerCase())) {
+                    Log.d(TAG, "found file " + file.getName());
+                    fileList.add(file.getName());
+                }
+            }
+        }
+        Log.d(TAG, "findAllFiles - finish");
     }
 
     private class OpenTask extends AsyncTask<Void, String, Void> {
@@ -43,6 +99,24 @@ public class OpenActivity extends SaveOpenActivity {
         @Override
         protected Void doInBackground(Void... params) {
             Log.d(TAG, "doInBackground - start");
+
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                } else {
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(activity,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_READWRITE_STORAGE);
+                    // READ_EXTERNAL_STORAGE is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }
 
 
             List<Model> models = new ArrayList<>();
@@ -110,26 +184,6 @@ public class OpenActivity extends SaveOpenActivity {
 
             Log.d(TAG, "doInBackground - finish");
             return null;
-        }
-
-        private void findAllFiles(List<String> fileList, String pattern) {
-            Log.d(TAG, "findAllFiles - start");
-            File sdPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            Log.d(TAG, "sdPath = " + sdPath + " :isExist = " + sdPath.exists() + " : isDirectory = " + sdPath.isDirectory());
-            File[] files = sdPath.listFiles();
-            if (files == null) {
-                Log.d(TAG, "findAllFiles - finish - No files");
-                return;
-            }
-            for (File file : files) {
-                if (!file.isDirectory()) {
-                    if (file.getName().endsWith(pattern.toLowerCase())) {
-                        Log.d(TAG, "found file " + file.getName());
-                        fileList.add(file.getName());
-                    }
-                }
-            }
-            Log.d(TAG, "findAllFiles - finish");
         }
 
         @Override
