6318b0655b9306ed6a6a7b51e828335971afd805
Konstantin8105 <pollygon2011>
2016-10-03 10:25:59 +0300

    fix bugs - tests is wrong

--- app/src/main/java/com/modelingbrain/home/detailModel/fragments/StageFragmentView.java
+++ app/src/main/java/com/modelingbrain/home/detailModel/fragments/StageFragmentView.java
@@ -29,9 +29,9 @@ public class StageFragmentView extends StageFragment {
         createElement(getResources().getString(R.string.model_name), QA.QUESTION);
         createElement(model.getName(), QA.ANSWER);
 
-        if (model.getModelID() == ModelID.ID_Polar) {
-            createPolar();
-        }
+//        if (model.getModelID() == ModelID.ID_Polar) {
+//            createPolar();
+//        }
 
         for (int i = 0; i < model.getModelID().getSize(); i++) {
             createElement(getResources().getStringArray(model.getModelID().getResourceQuestion())[1 + i], QA.QUESTION);
@@ -44,46 +44,47 @@ public class StageFragmentView extends StageFragment {
         return model;
     }
 
-    private void createPolar() {
-        LayoutInflater ltInflater = getActivity().getLayoutInflater();
-        View table = ltInflater.inflate(R.layout.view_model_polar, linLayout, false);
-
-        String[] array = getResources().getStringArray(model.getModelID().getResourceQuestion());
-
-        ((TextView) table.findViewById(R.id.textView1)).setText("");
-
-        //Axe -X
-        ((TextView) table.findViewById(R.id.textView2)).setText(
-                array[2] + "\n" + model.getAnswer(1));
-
-        //Axe +X
-        ((TextView) table.findViewById(R.id.textView3)).setText(
-                array[1] + "\n" + model.getAnswer(0));
-
-        //Axe -Y
-        ((TextView) table.findViewById(R.id.textView4)).setText(
-                array[4] + "\n" + model.getAnswer(3));
-
-        //Quadrant (-X, -Y)
-        ((TextView) table.findViewById(R.id.textView5)).setText(
-                model.getAnswer(7));
-
-        //Quadrant (+X, -Y)
-        ((TextView) table.findViewById(R.id.textView6)).setText(
-                model.getAnswer(6));
-
-        //Axe +Y
-        ((TextView) table.findViewById(R.id.textView7)).setText(
-                array[3] + "\n" + model.getAnswer(2));
-
-        //Quadrant (-X, +Y)
-        ((TextView) table.findViewById(R.id.textView8)).setText(
-                model.getAnswer(5));
-
-        //Quadrant (+X, +Y)
-        ((TextView) table.findViewById(R.id.textView9)).setText(
-                model.getAnswer(4));
-
-        linLayout.addView(table);
-    }
+    // TODO: 03.10.2016 if add polar and rotAte device - then few texts is not view.
+//    private void createPolar() {
+//        LayoutInflater ltInflater = getActivity().getLayoutInflater();
+//        View table = ltInflater.inflate(R.layout.view_model_polar, linLayout, false);
+//
+//        String[] array = getResources().getStringArray(model.getModelID().getResourceQuestion());
+//
+//        ((TextView) table.findViewById(R.id.textView1)).setText("");
+//
+//        //Axe -X
+//        ((TextView) table.findViewById(R.id.textView2)).setText(
+//                array[2] + "\n" + model.getAnswer(1));
+//
+//        //Axe +X
+//        ((TextView) table.findViewById(R.id.textView3)).setText(
+//                array[1] + "\n" + model.getAnswer(0));
+//
+//        //Axe -Y
+//        ((TextView) table.findViewById(R.id.textView4)).setText(
+//                array[4] + "\n" + model.getAnswer(3));
+//
+//        //Quadrant (-X, -Y)
+//        ((TextView) table.findViewById(R.id.textView5)).setText(
+//                model.getAnswer(7));
+//
+//        //Quadrant (+X, -Y)
+//        ((TextView) table.findViewById(R.id.textView6)).setText(
+//                model.getAnswer(6));
+//
+//        //Axe +Y
+//        ((TextView) table.findViewById(R.id.textView7)).setText(
+//                array[3] + "\n" + model.getAnswer(2));
+//
+//        //Quadrant (-X, +Y)
+//        ((TextView) table.findViewById(R.id.textView8)).setText(
+//                model.getAnswer(5));
+//
+//        //Quadrant (+X, +Y)
+//        ((TextView) table.findViewById(R.id.textView9)).setText(
+//                model.getAnswer(4));
+//
+//        linLayout.addView(table);
+//    }
 }
