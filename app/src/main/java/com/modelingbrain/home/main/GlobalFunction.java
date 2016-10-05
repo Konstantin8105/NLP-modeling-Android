5ec1b6ab566b3c867c9aed283eba1a11005e5635
konstantin8105 <konstantin8105@gmail.com>
2016-10-05 22:44:54 +0300

    bug fix - more speed in global function pause

--- app/src/main/java/com/modelingbrain/home/main/GlobalFunction.java
+++ app/src/main/java/com/modelingbrain/home/main/GlobalFunction.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 public class GlobalFunction {
     public static void pause() {
         try {
-            Thread.sleep(200);
+            Thread.sleep(20);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
