80a5e2b6800f522a046f1e1a7151264a003e0700
konstantin8105 <konstantin8105@gmail.com>
2016-10-05 21:35:11 +0300

    bug fix - modify colors

--- app/src/main/java/com/modelingbrain/home/detailModel/DetailActivity.java
+++ app/src/main/java/com/modelingbrain/home/detailModel/DetailActivity.java
@@ -34,7 +34,6 @@ public class DetailActivity extends AppCompatActivity {
 
     private FragmentType.Type fragmentType;
 
-
     private final String detailFragmentsKey = "FragmentType";
 
     @Override
